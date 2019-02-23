cd406cc181ca6956c66ab0d18a1f3b28c155878e
dKosarevsky <kosarevsky.d@ya.ru>
2019-02-23 08:32:08 +0300

    parsing emails from links in file

---
+++ parsemls.py
@@ -0,0 +1,52 @@
+'''
+Написать код программы для парсинга email-адресов с сайта. На входе файл с URL (URLs) для парсинга, на выходе csv-файл с емейлами.
+'''
+
+import requests
+from bs4 import BeautifulSoup
+import csv
+# import re
+
+
+def write_csv(data):
+    with open('emails.csv', 'a') as file:
+        writer = csv.writer(file)
+        writer.writerow([data])
+
+
+def get_html(url):
+    r = requests.get(url)
+    return r.text
+
+
+def get_emails(html):
+    soup = BeautifulSoup(html, 'lxml')
+    for i in soup.select("a[href^='mailto:']"):
+        dirty_mails = []
+        dirty_mails.append(i['href'])
+        for mail in dirty_mails:
+            clean = mail.split(':')
+            emails = []
+            emails.append(clean[1])
+            str_emails = ''.join(emails)
+        # print(str_emails)
+        write_csv(str_emails)
+
+
+    # emails = soup.find_all(text=re.compile('/@/'))
+    # for i in emails:
+    #     print(i.text)
+
+
+def main():
+    urls = []
+    with open ('urls.txt', 'r') as file:
+        for line in file:
+            urls.append(line.rstrip())
+
+    for url in urls:
+        get_emails(get_html(url))
+
+
+if __name__ == '__main__':
+    main()
